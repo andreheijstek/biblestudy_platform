a0af5db14836ed69743ab8b76f96b71770a243f9
Andre Heijstek <andre.heijstek@improvementfocus.com>
2016-07-08 12:06:39 +0200

    Add title functionality for biblestudy show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def title(*parts)
+    unless parts.empty?
+      content_for :title do
+        (parts << "Biblestudy-platform").join(" - ")
+      end
+    end
+  end
 end
--- spec/features/creating_bible_verses_spec.rb
+++ spec/features/creating_bible_verses_spec.rb
@@ -8,5 +8,10 @@ feature "Admins can create new bible sections" do
     fill_in "Description", with: "De handelingen der apostelen"
     click_button "Create Biblebook"
     expect(page).to have_content "Biblebook has been created."
+
+    book = Biblebook.find_by(name: "Handelingen")
+    expect(page.current_url).to eq biblebook_url(book)
+    title = "Handelingen - Biblebooks - Biblestudy-platform"
+    expect(page).to have_title title
   end
 end
