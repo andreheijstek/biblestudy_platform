76d34533591746fe051050117f28c193db057980
Andre Heijstek <andre.heijstek@improvementfocus.com>
2016-08-04 23:05:26 +0200

    Add all biblebooks to the database (dev only)

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,17 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-["Mattheüs", "Marcus", "Lucas", "Johannes"].each do |name|
+["Genesis", "Exodus", "Leviticus", "Numeri", "Deuteronomium", "Jozua", "Richteren", "Ruth",
+ "1 Samuël", "2 Samuël", "1 Koningen", "2 Koningen", "1 Kronieken", "2 Kronieken", "Ezra",
+ "Nehemia", "Esther", "Job", "Psalmen", "Spreuken", "Prediker", "Hooglied", "Jesaja", "Jeremia",
+ "Klaagliederen", "Ezechiël", "Daniël", "Hosea", "Joël", "Amos", "Obadja", "Jona", "Micha", "Nahum",
+ "Habakuk", "Sefanja", "Haggai", "Zacharia", "Maleachi", "Mattheüs", "Markus", "Lukas", "Johannes",
+ "Handelingen", "Romeinen", "1 Korintiërs", "2 Korintiërs", "Galaten", "Efeziërs", "Filippenzen",
+ "Kolossenzen", "1 Tessalonicenzen", "2 Tessalonicenzen", "1 Timotheüs", "2 Timotheüs", "Titus",
+ "Filemon", "Hebreeën", "Jakobus", "1 Petrus", "2 Petrus", "1 Johannes", "2 Johannes", "3 Johannes",
+ "Judas", "Openbaring"].each do |name|
   unless Biblebook.exists?(name: name)
-    Biblebook.create!(name: name, description: "Het evangelie naar de beschrijving van #{name}."
-    )
+    Biblebook.create!(name: name)
   end
 end
+
