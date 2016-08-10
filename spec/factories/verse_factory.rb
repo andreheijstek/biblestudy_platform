ce5c3672b9ce32266d32e4cd1963952b7d505b6f
Andre Heijstek <andre.heijstek@improvementfocus.com>
2016-08-10 10:42:49 +0200

    Add test for viewing verses (yes, I know, bad boy, after creating the software)

---
+++ spec/factories/verse_factory.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :verse do
+    verse_number '0'
+    verse_text 'Example verse text'
+  end
+end
--- spec/features/verses/viewing_verses_spec.rb
+++ spec/features/verses/viewing_verses_spec.rb
@@ -1,16 +1,20 @@
 require "rails_helper"
 
-RSpec.feature "Admins can view all chapters within biblebooks" do
+RSpec.feature "Users can view verses" do
 
-  scenario "for a given biblebook" do
-    booktitle = "Handelingen"
-    book = FactoryGirl.create(:biblebook, name: booktitle)
-    chapter = FactoryGirl.create(:chapter, biblebook: book,
-                                  chapter_number: '1',
-                                  description: 'Inleiding')
-    visit biblebook_path(book)
-
-    expect(page).to have_content "1 - Inleiding"
+  before do
+    book    = FactoryGirl.create(:biblebook, name: "testboek")
+    chapter = FactoryGirl.create(:chapter, biblebook: book, chapter_number: '1')
+    verse   = FactoryGirl.create(:verse, chapter: chapter, verse_number: '1', verse_text: "test vers")
+    ensure_on('/')
   end
 
+  scenario "for a given chapter" do
+    click_link "testboek"
+    expect(page).to have_content "testboek"
+    click_link "1"
+    within("#verses") do
+      expect(page).to have_content "test vers"
+    end
+  end
 end
