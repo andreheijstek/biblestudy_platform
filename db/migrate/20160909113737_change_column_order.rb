27639095ee26013dc2fe0c56b37a58ec61f298d5
Andre Heijstek <andre.heijstek@improvementfocus.com>
2016-09-09 13:56:54 +0200

    Fix bug with saving notes and pericopes

---
+++ db/migrate/20160909113737_change_column_order.rb
@@ -0,0 +1,21 @@
+class ChangeColumnOrder < ActiveRecord::Migration
+  def change
+    def up
+      change_table :biblebooks do |t|
+        t.change :testament, :text, after: :name
+        t.change :booksequence, :integer, after: :testament
+      end
+      change_table :chapters do |t|
+        t.change :nrofverses, :integer, after: :description
+      end
+      change_table :pericopes do |t|
+        t.change :name, :text, after: :id
+        t.change :starting_chapter_nr, :integer, after: :name
+        t.change :ending_chapter_nr, :integer, after: :starting_chapter_nr
+      end
+      change_table :study_notes do |t|
+        t.change :title, :text, after: :id
+      end
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160909112552) do
+ActiveRecord::Schema.define(version: 20160909113737) do
 
   create_table "biblebooks", force: :cascade do |t|
     t.string   "name"
