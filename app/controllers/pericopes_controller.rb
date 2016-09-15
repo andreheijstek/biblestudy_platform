81570673a4963dbba0e81f1392b85c0656920ddb
Andre Heijstek <andre.heijstek@improvementfocus.com>
2016-09-15 15:52:58 +0200

    Add studynote / pericope overview page, sorted by biblebook/pericope

--- app/controllers/pericopes_controller.rb
+++ app/controllers/pericopes_controller.rb
@@ -1,6 +1,8 @@
 class PericopesController < ApplicationController
   def index
-    @pericope = Pericope.all.order(:name)
-    # @study_note = StudyNote.all
+    @pericope = Pericope.all.order(:biblebook_id)
+
+    # This is a dirty trick. I should sort on biblebook.booksequence
+    # And thus do some joining to get the biblebooks, and from there the sequence
   end
 end
--- spec/factories/pericope_by_name_factory.rb
+++ spec/factories/pericope_by_name_factory.rb
@@ -1,9 +1,7 @@
 # The caller should create the correct biblebook before calling Pericope_xxx_Factory
-=begin
 
 FactoryGirl.define do
   factory :pericope_by_name, class: Pericope do
     name 'Handelingen 1:2 - 3:4'
   end
 end
-=end
