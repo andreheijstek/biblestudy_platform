6dde33ae01150e9ce14f1c73f1ea595e5bd32c1e
Andre Heijstek <andre.heijstek@improvementfocus.com>
2016-10-01 21:40:16 +0200

    Add placeholder for pericope#name and provide better error messages for pericope validations

--- app/models/pericope.rb
+++ app/models/pericope.rb
@@ -1,6 +1,8 @@
 class Pericope < ActiveRecord::Base
   belongs_to :studynote
   belongs_to :biblebook
+
+  validates :name,  presence: true
   validates_with PericopeValidator
 
   def bookname
--- app/models/pericope_validator.rb
+++ app/models/pericope_validator.rb
@@ -1,13 +1,17 @@
 class PericopeValidator < ActiveModel::Validator
   def validate(record)
     if record.name.nil? || record.name.empty?
-      record.errors[:base] << "Name can't be empty or nil"
+      record.errors[:name] << "Name can't be empty or nil"
       return
     end
 
     pericope = PericopeString.new(record.name)
     biblebook_name    = pericope.biblebook_name
     biblebook         = Biblebook.find_by(name: biblebook_name)
+    if biblebook.nil?
+      record.errors[:name] << "Unknown biblebook"
+      return
+    end
     record.biblebook_id = biblebook.id
 
     record.starting_chapter_nr = pericope.starting_chapter
@@ -16,12 +20,12 @@ class PericopeValidator < ActiveModel::Validator
     record.ending_verse        = pericope.ending_verse
 
     if record.starting_chapter_nr > record.ending_chapter_nr
-      record.errors[:base] << "Starting chapter can't be greater than ending chapter"
+      record.errors[:name] << "Starting chapter can't be greater than ending chapter"
       return
     end
 
     if (record.starting_chapter_nr == record.ending_chapter_nr) && (record.starting_verse > record.ending_verse)
-      record.errors[:base] << "Starting verse can't be greater than ending verse within the same chapter"
+      record.errors[:name] << "Starting verse can't be greater than ending verse within the same chapter"
       return
     end
   end
