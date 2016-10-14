f4ea9c722d46d7f660e9849eaece3f6b79b830cc
Andre Heijstek <andre.heijstek@improvementfocus.com>
2016-10-14 19:35:52 +0200

    Minor fix in authorization, and minor fix in layout of main index page

--- app/controllers/studynotes_controller.rb
+++ app/controllers/studynotes_controller.rb
@@ -1,6 +1,6 @@
 class StudynotesController < ApplicationController
 
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:edit, :update, :destroy]
   before_action :set_studynote, only: [:show, :edit, :update, :destroy]
 
   def index
