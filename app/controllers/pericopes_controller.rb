70b8f47328548f7429b0013c41f95e68c8eb5cc9
Andre Heijstek <andre.heijstek@improvementfocus.com>
2016-11-02 22:29:05 +0100

    Add Mailgun configuration

--- app/controllers/pericopes_controller.rb
+++ app/controllers/pericopes_controller.rb
@@ -3,10 +3,10 @@ class PericopesController < ApplicationController
     @pericopes = Pericope.all
     @pericope = []
     @pericopes.each do |pericope|
-      book = Biblebook.find(pericope.biblebook_id)
-      booksequence = book.booksequence
-      studynote = Studynote.find(pericope.studynote_id)
-      @pericope << {sequence: booksequence, book: pericope.name, author: studynote.author.username, title: studynote.title, note: studynote.note}
+      booksequence = Biblebook.find(pericope.biblebook_id).booksequence
+      studynote    = Studynote.find(pericope.studynote_id)
+      @pericope << {sequence: booksequence, pericope: pericope.name, author: studynote.author.username,
+                    title: studynote.title, note: studynote.note}
     end
     @pericope.sort_by! { |hsh| hsh[:sequence] }
   end
