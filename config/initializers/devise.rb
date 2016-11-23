0122d2d8d42455b8495ee577798cbb154311542c
Andre Heijstek <andre.heijstek@improvementfocus.com>
2016-11-23 10:45:10 +0100

    Change mailer address

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'www.bijbelstudie-platform.nl'
+  config.mailer_sender = 'bijbelstudie-platform <no-reply@bijbelstudie-platform.nl>'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
