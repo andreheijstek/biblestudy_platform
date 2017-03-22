787645a4b26958167bdba7ae93df8449f8271fef
Andre Heijstek <andre.heijstek@improvementfocus.com>
2017-03-22 22:18:09 +0100

    Refactoring on code inspections and i18n

--- app/controllers/pericopes_controller.rb
+++ app/controllers/pericopes_controller.rb
@@ -1,9 +1,10 @@
 class PericopesController < ApplicationController
   def index
-    @ot = Biblebook.where(testament: "oud"  ).select("name")
-    @nt = Biblebook.where(testament: "nieuw").select("name")
+    @ot = Biblebook.where(testament: 'oud').select('name')
+    @nt = Biblebook.where(testament: 'nieuw').select('name')
 
     @biblebook_counts = Pericope.group(:biblebook_name).count
-    @testament_counts = Pericope.joins(:biblebook).group("biblebooks.testament").count
+    @testament_counts = Pericope.joins(:biblebook)
+                                .group('biblebooks.testament').count
   end
 end
