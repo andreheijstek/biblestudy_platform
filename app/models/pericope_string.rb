17b4ff3710c585b8d6bd2ed88cef83257abd3f30
Andre Heijstek <andre.heijstek@improvementfocus.com>
2017-10-21 11:45:32 +0200

    Refactor pericope_string, using fewer instance variables, many could be turned into locals, partly due to using the attr_reader that was already there

--- app/models/pericope_string.rb
+++ app/models/pericope_string.rb
@@ -10,37 +10,38 @@ class PericopeString < String
     @starting_verse   = 0
     @ending_verse     = 0
 
-    @pericope_to_publish = StringScanner.new self
     parse
   end
 
   private
 
   def parse
-    @biblebook_name = @pericope_to_publish.scan(/(\d +\p{Word}+)|\p{Word}+/)  # e.g. 1 Samuel or Samuel
-    return if @pericope_to_publish.eos?
-    @biblebook_name = titleize(@biblebook_name)
+    pericope_to_publish = StringScanner.new self
 
-    @starting_chapter = @pericope_to_publish.scan(/\s+\d+\s*/).strip.to_i
+    @biblebook_name = pericope_to_publish.scan(/(\d +\p{Word}+)|\p{Word}+/)  # e.g. 1 Samuel or Samuel
+    return if pericope_to_publish.eos?
+    @biblebook_name = titleize(biblebook_name) # Todo: refactor, dit is procedureel, OO zou zijn @biblebook_name.titleize
 
-    if @pericope_to_publish.check(/:/)                                 # is a verse included after the chapter?
-      @pericope_to_publish.scan(/:/)                                   # skip the separator
-      @starting_verse = @pericope_to_publish.scan(/\s*\d+\s*/).strip.to_i
+    @starting_chapter = pericope_to_publish.scan(/\s+\d+\s*/).strip.to_i
+
+    if pericope_to_publish.check(/:/)                                 # is a verse included after the chapter?
+      pericope_to_publish.scan(/:/)                                   # skip the separator
+      @starting_verse = pericope_to_publish.scan(/\s*\d+\s*/).strip.to_i
     end
 
-    if @pericope_to_publish.check(/\s*-/)                              # is there an ending chapter or verse?
-      @pericope_to_publish.scan(/\s*-/)                                # skip the separator
-      if @pericope_to_publish.check(/\s*\d+:/)                         # is there a chapter AND a verse?
-        @ending_chapter = @pericope_to_publish.scan(/\s*\d+/).strip.to_i
-        @pericope_to_publish.scan(/:\s*/)                              # skip the separator
-        @ending_verse   = @pericope_to_publish.scan(/\d+/).strip.to_i
+    if pericope_to_publish.check(/\s*-/)                              # is there an ending chapter or verse?
+      pericope_to_publish.scan(/\s*-/)                                # skip the separator
+      if pericope_to_publish.check(/\s*\d+:/)                         # is there a chapter AND a verse?
+        @ending_chapter = pericope_to_publish.scan(/\s*\d+/).strip.to_i
+        pericope_to_publish.scan(/:\s*/)                              # skip the separator
+        @ending_verse   = pericope_to_publish.scan(/\d+/).strip.to_i
       else
-        @ending_chapter = @starting_chapter
-        @ending_verse   = @pericope_to_publish.scan(/\s*\d+/).strip.to_i
+        @ending_chapter = starting_chapter
+        @ending_verse   = pericope_to_publish.scan(/\s*\d+/).strip.to_i
       end
     else
-      @ending_chapter = @starting_chapter
-      @ending_verse   = @starting_verse
+      @ending_chapter = starting_chapter
+      @ending_verse   = starting_verse
     end
   end
 
