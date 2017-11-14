956c73b52253ee92924e6663efc5014b08909a54
Andre Heijstek <andre.heijstek@improvementfocus.com>
2017-11-14 21:49:42 +0100

    Fix bug with syntax error

--- spec/features/pericopes/pericope_spec.rb
+++ spec/features/pericopes/pericope_spec.rb
@@ -1,12 +1,14 @@
-frozen_string_literal: true
+# frozen_string_literal: true
 require 'rails_helper'
 
 feature 'Users can delete pericopes' do
-  let(:user) { create(:user) }
-  let(:studynote) { create(:studynote,
-                           pericope: 'Jona 1:1-5',
-                           title: 'Jona met 1 pericoop',
-                           note: 'zomaar iets')}
+  let(:user) {create(:user)}
+  let(:studynote) do
+    create(:studynote,
+           pericope: 'Jona 1:1-5',
+           title: 'Jona met 1 pericoop',
+           note: 'zomaar iets')
+  end
 
   before do
     create(:biblebook, name: 'Jona')
@@ -15,17 +17,16 @@ feature 'Users can delete pericopes' do
     click_link t(:new_studynote)
   end
 
-  scenario 'pericopes' do
-
-    it 'should be possible to delete a pericope' do
-      # vooraf moet er al een sn met 2 pericopen zijn (let)
-      # daar ga ik naartoe, visit :deze_sn
-      # daarvan delete ik er 1 (pericoop)
-      # dat moet dan goed gaan, dus in de show zie ik nog maar 1 pericoop, message=succes
-      expect(true).to eq(false)
-    end
-
-    it 'should not be possible to delete the only pericope' do
-
-    end
+  scenario 'it should be possible to delete a pericope', js: true do
+    click_on 'Voeg nog een pericoop toe'
+    should_see 'pericoop 2'
+    fill_in 'pericoop 2', with: 'Jona 2:20 - 3:3'
+    save_and_open_page
+    # vooraf moet er al een sn met 2 pericopen zijn (let)
+    # daar ga ik naartoe, visit :deze_sn
+    # daarvan delete ik er 1 (pericoop)
+    # dat moet dan goed gaan, dus in de show zie ik nog maar 1 pericoop, message=succes
+    # expect(true).to eq(false)
+  end
 end
+
