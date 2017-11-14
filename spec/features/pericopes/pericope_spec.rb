59c3694402ec72a65962077617c519c413dfc3f6
Andre Heijstek <andre.heijstek@improvementfocus.com>
2017-11-14 21:32:32 +0100

    Fix problem with Geckodriver locally and uncomment js spec

--- spec/features/pericopes/pericope_spec.rb
+++ spec/features/pericopes/pericope_spec.rb
@@ -1,31 +1,31 @@
-# frozen_string_literal: true
-# require 'rails_helper'
-#
-# feature 'Users can delete pericopes' do
-#   let(:user) { create(:user) }
-#   let(:studynote) { create(:studynote,
-#                            pericope: 'Jona 1:1-5',
-#                            title: 'Jona met 1 pericoop',
-#                            note: 'zomaar iets')}
-#
-#   before do
-#     create(:biblebook, name: 'Jona')
-#     login_as(user)
-#     visit studynotes_path
-#     click_link t(:new_studynote)
-#   end
-#
-#   scenario 'pericopes' do
-#
-#     it 'should be possible to delete a pericope' do
-#       # vooraf moet er al een sn met 2 pericopen zijn (let)
-#       # daar ga ik naartoe, visit :deze_sn
-#       # daarvan delete ik er 1 (pericoop)
-#       # dat moet dan goed gaan, dus in de show zie ik nog maar 1 pericoop, message=succes
-#       expect(true).to eq(false)
-#     end
-#
-#     it 'should not be possible to delete the only pericope' do
-#
-#     end
-# end
+frozen_string_literal: true
+require 'rails_helper'
+
+feature 'Users can delete pericopes' do
+  let(:user) { create(:user) }
+  let(:studynote) { create(:studynote,
+                           pericope: 'Jona 1:1-5',
+                           title: 'Jona met 1 pericoop',
+                           note: 'zomaar iets')}
+
+  before do
+    create(:biblebook, name: 'Jona')
+    login_as(user)
+    visit studynotes_path
+    click_link t(:new_studynote)
+  end
+
+  scenario 'pericopes' do
+
+    it 'should be possible to delete a pericope' do
+      # vooraf moet er al een sn met 2 pericopen zijn (let)
+      # daar ga ik naartoe, visit :deze_sn
+      # daarvan delete ik er 1 (pericoop)
+      # dat moet dan goed gaan, dus in de show zie ik nog maar 1 pericoop, message=succes
+      expect(true).to eq(false)
+    end
+
+    it 'should not be possible to delete the only pericope' do
+
+    end
+end
