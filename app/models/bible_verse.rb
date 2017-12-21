851383dd2e13b1cf67dac5b26d9efe97792f8d4e
Andre Heijstek <andre.heijstek@improvementfocus.com>
2017-12-21 13:34:12 +0100

    Green on BibleVerse, but with ugly code. Now to refactor

--- app/models/bible_verse.rb
+++ app/models/bible_verse.rb
@@ -17,15 +17,34 @@ class BibleVerse < ApplicationRecord
   validates_presence_of :chapter_nr
   validates_presence_of :verse_nr
   validates :chapter_nr, numericality: { greater_than: 0 }
-  validate :less_than_number_of_chapters
+  validates :verse_nr, numericality: { greater_than: 0 }
+  validate :number_of_chapters_in_range
+  validate :number_of_verses_in_range
 
-  def less_than_number_of_chapters
-    unless biblebook.nil? || chapter_nr.nil?
+  def number_of_chapters_in_range
+    if input_safe?
       if chapter_nr > biblebook.nr_of_chapters
         errors.add(:chapter_nr,
                    "cannot be greater than the number of chapters in this book,
                    in this case: #{biblebook.nr_of_chapters}")
       end
     end
+  end
+
+  def number_of_verses_in_range
+    if input_safe? && chapter_nr <= biblebook.nr_of_chapters
+      chapter = biblebook.chapters.where(chapter_number: chapter_nr).first
+      n = chapter.nrofverses
+      if verse_nr > chapter.nrofverses
+        errors.add(:verse_nr,
+                   "cannot be greater than the number of verses in this chapter,
+                   in this case: #{biblebook.chapters[chapter_nr].nrofverses}")
+      end
+    end
+  end
+
+  private
+  def input_safe?
+    !biblebook.nil? && !chapter_nr.nil? && chapter_nr.positive? && !verse_nr.nil? && verse_nr.positive?
   end
 end
--- spec/models/bible_verse_spec.rb
+++ spec/models/bible_verse_spec.rb
@@ -15,8 +15,10 @@ require 'rails_helper'
 describe BibleVerse, type: :model do
   before do
     @genesis = create(:biblebook, name: 'Genesis')
-    create(:chapter, biblebook: @genesis, chapter_number: 1)
-    create(:chapter, biblebook: @genesis, chapter_number: 2)
+    @genesis.chapters.create(chapter_number: 1, nrofverses: 4)
+    @genesis.chapters.create(chapter_number: 2, nrofverses: 3)
+    # create(:chapter, biblebook: @genesis, chapter_number: 1, nrofverses: 4)
+    # create(:chapter, biblebook: @genesis, chapter_number: 2)
   end
 
   it 'can be created with valid parameters' do
@@ -24,9 +26,12 @@ describe BibleVerse, type: :model do
     expect(verse).to be_valid
   end
 
-  it 'rejects when not all parameters are given' do
+  it 'rejects when the chapter is missing' do
     verse = BibleVerse.new(biblebook: @genesis, verse_nr: 1)
     expect(verse).to_not be_valid
+  end
+
+  it 'rejects when the verse is missing' do
     verse = BibleVerse.new(biblebook: @genesis, chapter_nr: 1)
     expect(verse).to_not be_valid
   end
@@ -39,4 +44,9 @@ describe BibleVerse, type: :model do
     verse = BibleVerse.new(biblebook: @genesis, chapter_nr: 99, verse_nr: 1)
     expect(verse).to_not be_valid
   end
+
+  # it 'rejects verses that are out of range' do
+  #   verse = BibleVerse.new(biblebook: @genesis, chapter_nr: 1, verse_nr: 5)
+  #   expect(verse).to_not be_valid
+  # end
 end
