c602cfa87a792f4adf557c2f5d959fb65512246c
Andre Heijstek <andre.heijstek@improvementfocus.com>
2018-02-15 08:08:27 +0100

    Solved the problem with Cocoon callback, was a problem in de config

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -41,4 +41,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   config.action_view.raise_on_missing_translations = true
+
+  config.assets.debug = true
+  config.assets.digest = true
+  config.assets.raise_runtime_errors = true
 end
