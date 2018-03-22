beaeaca64f85aa56d3459e136d627fc048639591
Andre Heijstek <andre.heijstek@improvementfocus.com>
2018-03-22 11:26:33 +0100

    Clean up mess after accedentally create migration for feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180319102439) do
+ActiveRecord::Schema.define(version: 20171223162059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,11 +42,6 @@ ActiveRecord::Schema.define(version: 20180319102439) do
     t.datetime "updated_at", null: false
     t.integer "nrofverses"
     t.index ["biblebook_id"], name: "index_chapters_on_biblebook_id"
-  end
-
-  create_table "features", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "pericope_as_ranges", force: :cascade do |t|
