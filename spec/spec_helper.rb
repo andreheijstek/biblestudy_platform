e2724aa6564dab77bb05792afc4bbb5ad83b4f41
Andre Heijstek <andre.heijstek@improvementfocus.com>
2018-06-15 10:52:50 +0200

    Add logging for slowest specs, so I can speed them up

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -98,4 +98,9 @@ RSpec.configure do |config|
   # test failures related to randomization by passing the same `--seed` value
   # as the one that triggered the failure.
   Kernel.srand config.seed
+
+  # Print the 10 slowest examples and example groups at the
+  # end of the spec run, to help surface which specs are running
+  # particularly slow.
+  config.profile_examples = 10
 end
