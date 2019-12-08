03370873702605919f5de0f6fc74490c46127c01
Andre Heijstek <andre.heijstek@improvementfocus.com>
2019-12-08 15:39:08 +0100

    Add config to chrome - hoping to fix tests on gitlab

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -100,11 +100,10 @@ RSpec.configure do |config|
 
   Capybara.register_driver :chrome do |app|
     options = Selenium::WebDriver::Chrome::Options.new(
-    args: %w[headless no-sandbox]
+    args: %w[headless no-sandbox disable-dev-shm-usage]
     )
     Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
   end
 
   Capybara.javascript_driver = :chrome
 end
-
