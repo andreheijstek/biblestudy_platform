1c9f220aac1c5f9f517967d161e71ecc29beed89
Andre Heijstek <andre.heijstek@improvementfocus.com>
2019-12-08 15:51:22 +0100

    Remove cucumber features, maybe they interfere with chrome / rspec

--- features/pages/login.rb
+++
@@ -1,7 +0,0 @@
-class Login < SitePrism::Page
-  set_url '/users/sign_in'
-
-  element :email_field, '#user_email'
-  element :password_field, '#user_password'
-  element :login_button, "input[name='commit']"
-end
--- features/step_definitions/login_steps.rb
+++
@@ -1,4 +0,0 @@
-Given("I am logged in") do
-  @user = create(:user)
-  login_as(@user)
-end
