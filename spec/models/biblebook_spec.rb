9041e05d273f9d919a77b881066d37786af50cac
Andre Heijstek <andre.heijstek@improvementfocus.com>
2020-02-08 22:22:36 +0100

    Improve handling of ambiguous biblebooks

--- spec/models/biblebook_spec.rb
+++ spec/models/biblebook_spec.rb
@@ -18,7 +18,6 @@ describe Biblebook do
   let(:genesis) { create(:biblebook, name: 'Genesis', abbreviation: 'Gen') }
 
   before do
-    # @genesis = create(:biblebook, name: 'Genesis')
     create(:chapter, biblebook_id: genesis.id, nrofverses: 5)
     create(:chapter, biblebook_id: genesis.id, nrofverses: 8)
   end
@@ -31,6 +30,9 @@ describe Biblebook do
     expect(genesis.chapter_valid?(1)).to eq(true)
   end
 
+  it 'rejects chapters outside range' do
+    expect(genesis.chapter_valid?(3)).to eq(false)
+  end
   it 'can find which book match a bookname when the full name is given' do
     expect(Biblebook.possible_book_names('Genesis')). to eq(['Genesis'])
   end
@@ -51,5 +53,29 @@ describe Biblebook do
     books = Biblebook.possible_book_names('Jo')
 
     expect(books). to eq(['Job', 'Johannes', 'Jona'])
+  end
+
+  it 'finds books by full name' do
+    book = Biblebook.find_by_full_name('Genesis')
+    expect(book.first).to eq(genesis)
+    # Note, .first is needed, because we can an AREL back, not an Object
+  end
+
+  it 'finds books by abbreviation' do
+    book = Biblebook.find_by_abbreviation('gen')
+    expect(book.first).to eq(genesis)
+  end
+
+  it 'finds single books by fuzzy name' do
+    book = Biblebook.find_names_by_like('Gene')
+    expect(book.first).to eq(genesis)
+  end
+
+  it 'finds multiple books by fuzzy name' do
+    create(:biblebook, name: 'Job')
+    create(:biblebook, name: 'Johannes')
+    create(:biblebook, name: 'Jona')
+    books = Biblebook.find_names_by_like('Jo')
+    expect(books.count).to eq(3)
   end
 end
--- spec/models/pericope_validator_spec.rb
+++ spec/models/pericope_validator_spec.rb
@@ -3,10 +3,25 @@
 require 'rails_helper'
 
 describe PericopeValidator do
-  let!(:gen) { create(:biblebook, name: 'Genesis', abbreviation: 'gen') }
+  before do
+    create(:biblebook, name: 'Genesis', abbreviation: 'gen')
+    create(:biblebook, name: 'Ezechiel', abbreviation: 'eze')
+    create(:biblebook, name: 'Ezra', abbreviation: 'ezr')
+  end
+
 
   it 'accepts a pericope of one verse' do
-    pericope = Pericope.new(name: 'gen 1:1')
+    pericope = Pericope.create(name: 'gen 1:1')
     expect(pericope).to be_valid
+  end
+
+  it 'finds the right biblebook' do
+    pericope = Pericope.create(name: 'gen 1:1')
+    expect(pericope.biblebook_name).to eq('Genesis')
+  end
+
+  it 'detects ambiguous biblebooks' do
+    pericope = Pericope.create(name: 'ez 1:1')
+    expect(pericope.errors.messages[:name].first).to eq('de afkorting is niet duidelijk genoeg: ez kan Ezechiel, Ezra zijn')
   end
 end
