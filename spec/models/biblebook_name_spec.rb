49117980c109e49a88160e97b8f40ea2b2743a76
Andre Heijstek <andre.heijstek@hey.com>
2021-10-25 19:52:20 +0200

    Add extra checks in the tests

--- spec/models/biblebook_name_spec.rb
+++ spec/models/biblebook_name_spec.rb
@@ -5,16 +5,19 @@ describe BiblebookName do
   it 'creates Names from strings that represent a real biblebook' do
     name = BiblebookName.create("Genesis")
     expect(name.class).to eq(BiblebookName)
+    expect(name.name).to eq("Genesis")
   end
 
   it 'creates Names from lowercase strings that represent a real biblebook ' do
     name = BiblebookName.create("genesis")
     expect(name.class).to eq(BiblebookName)
+    expect(name.name).to eq("Genesis")
   end
 
   it 'creates Names from uppercase strings that represent a real biblebook ' do
     name = BiblebookName.create("GENESIS")
     expect(name.class).to eq(BiblebookName)
+    expect(name.name).to eq("Genesis")
   end
 
   it 'creates Names from multiple strings that represent a real biblebook' do
@@ -25,26 +28,31 @@ describe BiblebookName do
   it 'creates Names from multiple lowercase strings that represent a real biblebook' do
     name = BiblebookName.create("1 johannes")
     expect(name.class).to eq(BiblebookName)
+    expect(name.name).to eq("1 Johannes")
   end
 
   it 'creates an UnknownBiblebook object for unknown strings' do
     name = BiblebookName.create("nonsense")
     expect(name.class).to eq(UnknownBiblebookName)
+    expect(name.name).to eq("Unknown biblebook")
   end
 
   it 'creates Names from official abbreviations' do
     name = BiblebookName.create("gen")
     expect(name.class).to eq(BiblebookName)
+    expect(name.name).to eq("Genesis")
   end
 
   it 'creates Names from uppercase official abbreviations' do
     name = BiblebookName.create("GEN")
     expect(name.class).to eq(BiblebookName)
+    expect(name.name).to eq("Genesis")
   end
 
   it 'creates Names from inofficial abbreviations' do
     name = BiblebookName.create("genes")
     expect(name.class).to eq(BiblebookName)
+    expect(name.name).to eq("Genesis")
   end
 
   it 'rejects Names from ambiguous abbreviations' do
