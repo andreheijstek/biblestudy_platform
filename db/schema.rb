30762d2176b6238b11168a4ee47745bacf720381
Andre Heijstek <andre.heijstek@hey.com>
2023-05-06 20:39:35 +0200

    Remove old BibleVerse model to allow the new model to enter

--- db/schema.rb
+++ db/schema.rb
@@ -10,33 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_13_213302) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_05_06_182419) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "bible_verses", force: :cascade do |t|
-    t.bigint "biblebook_id"
-    t.integer "chapter_nr"
-    t.integer "verse_nr"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["biblebook_id"], name: "index_bible_verses_on_biblebook_id"
-  end
 
   create_table "biblebook_categories", force: :cascade do |t|
     t.string "name"
     t.integer "order"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.bigint "biblebook_id"
     t.index ["biblebook_id"], name: "index_biblebook_categories_on_biblebook_id"
   end
 
   create_table "biblebooks", id: :serial, force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.integer "booksequence"
     t.string "testament"
     t.string "abbreviation"
@@ -48,8 +38,8 @@ ActiveRecord::Schema.define(version: 2019_03_13_213302) do
   create_table "categories", force: :cascade do |t|
     t.text "name"
     t.bigint "biblebook_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["biblebook_id"], name: "index_categories_on_biblebook_id"
   end
 
@@ -57,8 +47,8 @@ ActiveRecord::Schema.define(version: 2019_03_13_213302) do
     t.integer "chapter_number"
     t.string "description"
     t.integer "biblebook_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.integer "nrofverses"
     t.index ["biblebook_id"], name: "index_chapters_on_biblebook_id"
   end
@@ -67,8 +57,8 @@ ActiveRecord::Schema.define(version: 2019_03_13_213302) do
     t.string "name"
     t.bigint "starting_verse_id"
     t.bigint "ending_verse_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["ending_verse_id"], name: "index_pericope_as_ranges_on_ending_verse_id"
     t.index ["starting_verse_id"], name: "index_pericope_as_ranges_on_starting_verse_id"
   end
@@ -78,8 +68,8 @@ ActiveRecord::Schema.define(version: 2019_03_13_213302) do
     t.integer "starting_verse"
     t.integer "ending_verse"
     t.integer "biblebook_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "name"
     t.integer "ending_chapter_nr"
     t.integer "starting_chapter_nr"
@@ -93,16 +83,16 @@ ActiveRecord::Schema.define(version: 2019_03_13_213302) do
     t.integer "user_id"
     t.string "role"
     t.integer "studynote_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["studynote_id"], name: "index_roles_on_studynote_id"
     t.index ["user_id"], name: "index_roles_on_user_id"
   end
 
   create_table "studynotes", id: :serial, force: :cascade do |t|
     t.text "note"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "title"
     t.integer "author_id"
     t.index ["author_id"], name: "index_studynotes_on_author_id"
@@ -112,15 +102,15 @@ ActiveRecord::Schema.define(version: 2019_03_13_213302) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
     t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
+    t.datetime "current_sign_in_at", precision: nil
+    t.datetime "last_sign_in_at", precision: nil
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.boolean "admin", default: false
     t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
@@ -131,17 +121,14 @@ ActiveRecord::Schema.define(version: 2019_03_13_213302) do
     t.integer "verse_number"
     t.string "verse_text"
     t.integer "chapter_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["chapter_id"], name: "index_verses_on_chapter_id"
   end
 
-  add_foreign_key "bible_verses", "biblebooks"
   add_foreign_key "biblebook_categories", "biblebooks"
   add_foreign_key "categories", "biblebooks"
   add_foreign_key "chapters", "biblebooks"
-  add_foreign_key "pericope_as_ranges", "bible_verses", column: "ending_verse_id"
-  add_foreign_key "pericope_as_ranges", "bible_verses", column: "starting_verse_id"
   add_foreign_key "pericopes", "biblebooks"
   add_foreign_key "pericopes", "studynotes"
   add_foreign_key "roles", "studynotes"
