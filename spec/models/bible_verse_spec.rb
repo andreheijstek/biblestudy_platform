fff6c69186a20aa7ac728ab40bc5e8a4fdba55bc
Andre Heijstek <andre.heijstek@hey.com>
2023-05-26 21:09:30 +0200

    Add model specs for bibleverse

--- spec/models/bible_verse_spec.rb
+++ spec/models/bible_verse_spec.rb
@@ -13,5 +13,31 @@
 require "rails_helper"
 
 RSpec.describe BibleVerse, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "recognises BibleVerses with equal chapters and verses to be equal" do
+    verse1 = BibleVerse.create(chapter: 1, verse: 2)
+    verse2 = BibleVerse.create(chapter: 1, verse: 2)
+
+    expect(verse1 == verse2).to be_truthy
+  end
+
+  it "recognises BibleVerses with different chapters and equal verses to be different" do
+    verse1 = BibleVerse.create(chapter: 1, verse: 2)
+    verse2 = BibleVerse.create(chapter: 2, verse: 2)
+
+    expect(verse1 == verse2).to be_falsey
+  end
+
+  it "recognises BibleVerses with equal chapters and different verses to be different" do
+    verse1 = BibleVerse.create(chapter: 1, verse: 1)
+    verse2 = BibleVerse.create(chapter: 1, verse: 2)
+
+    expect(verse1 == verse2).to be_falsey
+  end
+
+  it "compares BibleVerses large and small" do
+    verse1 = BibleVerse.create(chapter: 1, verse: 2)
+    verse2 = BibleVerse.create(chapter: 1, verse: 3)
+
+    expect(verse1 < verse2).to be_truthy
+  end
 end
