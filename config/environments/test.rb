8f44921a630a1aa7d6aa473779e97ffb40572a43
Andre Heijstek <andre.heijstek@hey.com>
2023-05-31 17:52:00 +0200

    Remove clutter from pericope index page

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -15,8 +15,6 @@ Rails.application.configure do
   # preloads Rails for running tests, you may have to set it to true.
   config.eager_load = false
 
-  config.log_level = :error
-
   # Configure static file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
