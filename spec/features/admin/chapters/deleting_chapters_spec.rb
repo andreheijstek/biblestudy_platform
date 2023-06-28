34d30028cd45eb8f077bbb5968a8456aa4e6f20d
Andre Heijstek <andre.heijstek@hey.com>
2023-06-28 13:09:15 +0200

    Fix more commented specs

--- spec/features/admin/chapters/deleting_chapters_spec.rb
+++ spec/features/admin/chapters/deleting_chapters_spec.rb
@@ -1,22 +1,18 @@
 # frozen_string_literal: true
 
-# TODO: Commented. Spec currently not working. It does not visit my
-# website but www.example.com.
-# Probably needs to be refactored using the page object model,
-# but for the admin pages I don't want to invest that time right now.
+feature "Users can delete chapters" do
+  let(:biblebook) { create(:biblebook) }
+  let(:chapter) { create(:chapter, biblebook: biblebook) }
 
-# feature "Users can delete chapters" do
-#   let(:biblebook) { create(:biblebook) }
-#   let(:chapter) { create(:chapter, biblebook: biblebook) }
-#
-#   before do
-#     login_as(create(:user, :admin))
-#     visit admin_biblebook_chapter_path(biblebook, chapter)
-#   end
-#
-#   scenario "successfully" do
-#     click_link t(:delete_item, item: Chapter.model_name.human)
-#     should_see t(:item_deleted, item: Chapter.model_name.human)
-#     expect(page.current_url).to eq admin_biblebook_url(biblebook)
-#   end
-# end
+  before do
+    login_as(create(:user, :admin))
+    visit admin_biblebook_chapter_path(biblebook, chapter)
+  end
+
+  scenario "successfully" do
+    page.accept_alert 'Weet u zeker dat u dit hoofdstuk wilt verwijderen?' do
+      click_link t(:delete_item, item: Chapter.model_name.human)
+    end
+    should_see t(:item_deleted, item: Chapter.model_name.human)
+  end
+end
