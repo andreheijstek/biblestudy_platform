d2970163136b0bb07dbe33b3c1b90c5f0d5a698e
Andre Heijstek <andre.heijstek@hey.com>
2023-08-27 22:38:28 +0200

    Setup for deploy to render, according to https://render.com/docs/deploy-rails#go-production-ready

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,5 +101,9 @@ Rails.application.configure do
   }
   config.action_mailer.default_url_options = { host: }
 
+  # Disable serving static files from the `/public` folder by default since
+  # Apache or NGINX already handles this.
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
+
   GOOGLE_ANALYTICS_TRACKING_ID = "UA-132209861-1"
 end
