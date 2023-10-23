2e2fb07e858152a7d09e1079320034c6045f09c8
Andre Heijstek <andre.heijstek@hey.com>
2023-10-23 08:26:20 +0200

    Move db backup to render

--- db/migrate/20231022202748_rename_bible_verse_attributes.rb
+++ db/migrate/20231022202748_rename_bible_verse_attributes.rb
@@ -2,13 +2,13 @@ class RenameBibleVerseAttributes < ActiveRecord::Migration[7.0]
   # This looks really stupid, but was needed. There was a mismatch in db schema between
   # development and production. This got it in sync again
   def change
-    if BibleVerse.column_names.include? 'chapter_nr'
+    if BibleVerse.column_names.include? "chapter_nr"
       rename_column :bible_verses, :chapter_nr, :chapter
     else
       rename_column :bible_verses, :chapter, :chapter_nr
     end
 
-    if BibleVerse.column_names.include? 'verse_nr'
+    if BibleVerse.column_names.include? "verse_nr"
       rename_column :bible_verses, :verse_nr, :verse
     else
       rename_column :bible_verses, :verse, :verse_nr
