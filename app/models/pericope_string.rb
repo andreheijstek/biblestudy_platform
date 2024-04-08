0f0fcd2a5a29fc46ce1b8a536838c5009227022e
Andre Heijstek <andre.heijstek@hey.com>
2024-04-08 08:11:09 +0200

    All specs in PericopeString passing

--- app/models/pericope_string.rb
+++ app/models/pericope_string.rb
@@ -1,12 +1,39 @@
 # frozen_string_literal: true
 
 # PORO class to handle pericopes
+# To keep this class at a single responsibility, this class only takes
+# care of basic handling of the pericope. It parses it into the constituent
+# parts, but does not handle further logic to complete abbreviated
+# biblebook names, or to check on valid chapters and verses
 class PericopeString
+  include PericopeValidatorUtilities
+
   def initialize(pericope)
     @pericope = pericope
+    @tree = PericopeParser.new.parse(pericope)
   end
 
   def to_s
     @pericope.to_s
+  end
+
+  def biblebook
+    get_biblebook_from_tree(@tree)
+  end
+
+  def first_chapter
+    @tree[:starting_chapter].to_i
+  end
+
+  def first_verse
+    @tree[:starting_verse].to_i
+  end
+
+  def last_chapter
+    @tree[:ending_chapter].to_i
+  end
+
+  def last_verse
+    @tree[:ending_verse].to_i
   end
 end
--- spec/models/pericope_string_spec.rb
+++ spec/models/pericope_string_spec.rb
@@ -1,23 +1,33 @@
 # frozen_string_literal: true
 
 describe PericopeString do
-  let(:pericope) { described_class.new("Genesis 1:2-3:4") }
+  let(:pericope) { described_class.new('Genesis 1:2-3:4') }
 
   it 'creates an object based on a pericope string' do
-    expect(pericope.class).to eq(PericopeString)
+    expect(pericope.class).to eq(described_class)
   end
 
   it 'returns a string version of the pericope object' do
-    expect(pericope.to_s).to eq("Genesis 1:2-3:4")
+    expect(pericope.to_s).to eq('Genesis 1:2-3:4')
   end
 
   it 'returns the biblebook part' do
-
+    expect(pericope.biblebook).to eq('Genesis')
   end
 
-  it 'returns the first chapter'
-  it 'returns the first verse'
-  it 'returns the last chapter'
-  it 'returns the last verse'
-  it 'reformats a partial pericope'
+  it 'returns the first chapter' do
+    expect(pericope.first_chapter).to eq(1)
+  end
+
+  it 'returns the first verse' do
+    expect(pericope.first_verse).to eq(2)
+  end
+
+  it 'returns the last chapter' do
+    expect(pericope.last_chapter).to eq(3)
+  end
+
+  it 'returns the last verse' do
+    expect(pericope.last_verse).to eq(4)
+  end
 end
